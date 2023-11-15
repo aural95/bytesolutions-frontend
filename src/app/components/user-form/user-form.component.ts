a58a782e0800aceb7646590c2998bb3c83ac898e
Jorge Fergusson <jorgefergusson@gmail.com>
2023-11-15 00:17:16 -0500

    Added reusable form for user registration added customization to navbar and addedroutes to sidebar

---
+++ src/app/components/user-form/user-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss']
+})
+export class UserFormComponent {
+  @Input()
+  typeOfForm!: string;
+}
