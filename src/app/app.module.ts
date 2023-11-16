44c47ccff98862204ffd1aa8421fcc90267b3636
Jorge Fergusson <jorgefergusson@gmail.com>
2023-11-16 00:53:54 -0500

    Finished UI for common form for new users and to edit users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ScheduleAppointmentComponent } from './schedule-appointment/schedule-appointment.component';
 import { LoadAvailabilityComponent } from './load-availability/load-availability.component';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +23,18 @@ import { LoadAvailabilityComponent } from './load-availability/load-availability
     LoginComponent,
     SidebarComponent,
     ScheduleAppointmentComponent,
-    LoadAvailabilityComponent
+    LoadAvailabilityComponent,
+    UsersComponent,
+    NavbarComponent,
+    UserRegistrationComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-form',
@@ -6,6 +10,52 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent {
+  
+  path:string="";
+  roles:any[]=[];
+  
   @Input()
   typeOfForm!: string;
+  
+  
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ){
+    
+  };
+  //Set the path to return when the user fill the form
+  ngOnInit(): void {
+    this.getRoleList();
+    console.log(this.roles);
+    if(this.typeOfForm==="Patient"){
+      this.path="/signup";
+    }else{
+      this.path="/users";
+    }
+  };
+
+  
+
+  //Function to send to the specific path after finishing 
+  returnToPreviousRoute():void{
+    this.router.navigate([this.path]);
+  };
+
+  getRoleList(){
+    let bodyData = {};
+    this.http
+      .get('http://localhost:4000/roles')
+      .subscribe((resultData: any) => {
+        if(resultData.status){
+          console.log(resultData);
+          this.roles=resultData.message;
+        }
+        else{
+          console.log("Error getting roles");
+        }
+      });
+  };
+
 }
