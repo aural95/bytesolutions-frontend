adfd7cb5ae79ff6e0a0ddfce4032c0ca2d149de6
Jose Cabrera <cabrerajosemiguel28@gmail.com>
2023-11-18 00:42:53 -0500

    adds login feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ScheduleAppointmentComponent } from './schedule-appointment/schedule-appointment.component';
 import { LoadAvailabilityComponent } from './load-availability/load-availability.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { LoadAvailabilityComponent } from './load-availability/load-availability
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  credentials = { email: '', password: '' };
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.credentials).subscribe(
+      (response) => {
+        console.log(response);
+        const token = response.data.token;
+        const user = response.data.user.fullname; 
+        let role = response.data.user.id_role == "655434872cdb661ebbfc9437" ? "patient" : "Undefined"; 
+        this.setCookie('token', token, 2);
+        sessionStorage.setItem("token",token);
+        sessionStorage.setItem("role",role);
+        sessionStorage.setItem("user",user);
+        this.router.navigate(['/schedule']);
+      },
+      (error) => {
+        alert(error);
+        console.error('Session Error:', error);
+      }
+    );
+  }
+  setCookie(name: string, value: any, hours: number) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + hours * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+  }
 }
