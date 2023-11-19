ce819e0b9da6f0d9fd6cceaefe44acd73bcb5fc4
Jorge Fergusson <jorgefergusson@gmail.com>
2023-11-18 20:15:03 -0500

    Finished listing users and show edit form with selected user information

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -5,55 +5,68 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+  styleUrls: ['./user-form.component.scss'],
 })
 export class UserFormComponent {
-  
-  path:string="";
-  roles:any[]=[];
-  
-  @Input()
-  typeOfForm!: string;
-  
-  
+  path: string = '';
+  roles: any[] = [];
+  email: string = '';
+  id_role: any = {name:""};
+  fullname: string = '';
+  birthdate: Date =  new Date;
+  gender: string = '';
+  healthcard: string = '';
+  password: string = '';
+  createdAt: Date | undefined;
+  updatedAt: Date | undefined;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-  ){
-    
-  };
+  @Input() typeOfForm!: string;
+  @Input() user!: any;
+
+  constructor(private http: HttpClient, private router: Router) {}
   //Set the path to return when the user fill the form
   ngOnInit(): void {
     this.getRoleList();
+
     console.log(this.roles);
-    if(this.typeOfForm==="Patient"){
-      this.path="/signup";
-    }else{
-      this.path="/users";
+    if (this.typeOfForm === 'Patient') {
+      this.path = '/signup';
+    } else {
+      this.path = '/users';
     }
-  };
+  }
 
-  
-
-  //Function to send to the specific path after finishing 
-  returnToPreviousRoute():void{
+  //Function to send to the specific path after finishing
+  returnToPreviousRoute(): void {
     this.router.navigate([this.path]);
-  };
+  }
 
-  getRoleList(){
+  getUserToEdit(user: any): void {
+    this.email = user.email;
+    this.id_role = user.id_role;
+    this.fullname = user.fullname;
+    this.birthdate = new Date(user.birthdate);    
+    this.gender = user.gender;
+    this.healthcard = user.healthcard;
+    this.password = user.password;
+    this.createdAt = user.createdAt;
+    this.updatedAt = user.updatedAt;
+  }
+
+  save():void{
+
+  }
+
+  getRoleList():void {
     let bodyData = {};
     this.http
-      .post('http://localhost:4000/roles',bodyData)
+      .get('http://localhost:4000/roles')
       .subscribe((resultData: any) => {
-        if(resultData.status){
-          console.log(resultData);
-          this.roles=resultData.message;
-        }
-        else{
-          console.log("Error getting roles");
+        if (resultData.status) {
+          this.roles = resultData.message;
+        } else {
+          console.log('Error getting roles');
         }
       });
-  };
-
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { UserFormComponent } from '../components/user-form/user-form.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+    
+  @ViewChild(UserFormComponent) userForm!:UserFormComponent;
+  public loadChild(): void {
+    
+  }
+  //Variable declarations
+  userList: any[]=[];
 
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers():void{
+    this.http
+      .get('http://localhost:4000/users')
+      .subscribe((resultData: any) => {
+        if (resultData.status) {
+          this.userList = resultData.message;
+        } else {
+          alert('Error');
+        }
+      });
+  }
+
+  userToBeEdited(user:any){
+    this.userForm.getUserToEdit(user);
+  }
+  
 }
