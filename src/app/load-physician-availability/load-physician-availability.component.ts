6650070756175e7f6809175c66935618ca6940b5
Michael <michael.silvav@hotmail.com>
2023-12-07 11:56:40 -0500

    Cambios finales BYT-23

--- src/app/load-physician-availability/load-physician-availability.component.ts
+++ src/app/load-physician-availability/load-physician-availability.component.ts
@@ -10,6 +10,8 @@ import listPlugin from '@fullcalendar/list';
 
 import { HttpClient } from '@angular/common/http';
 
+
+
 @Component({
   selector: 'app-load-physician-availability',
   templateUrl: './load-physician-availability.component.html',
@@ -23,7 +25,6 @@ export class LoadPhysicianAvailabilityComponent {
   ngOnInit(): void{ 
     
     this.update();
-   //this.obtenerEventos();
     
    }
 
@@ -34,9 +35,6 @@ export class LoadPhysicianAvailabilityComponent {
     aspectRatio: 1.7,
     weekends: false,
     initialView: 'dayGridMonth',
-    //
-    //events: this.events,
-    //
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin ],
     headerToolbar: {
       left: 'prev,next today',
@@ -50,12 +48,9 @@ export class LoadPhysicianAvailabilityComponent {
     events: [
       {
         title: 'My Event',
-        //date: '2023-11-22',
-       // color: '#0000FF',// elimino los
-        
         start: '2023-12-01T10:00:00', 
         end: '2023-12-01T12:00:00',   
-        constraint: 'businessHours' // DESCOMENTE ESTO
+        constraint: 'businessHours' 
       }
     ],
    businessHours: true,
@@ -67,32 +62,36 @@ export class LoadPhysicianAvailabilityComponent {
 
   update(){
     console.log("entrando a update");
+    console.log(this.calendarOptions.events);
     
     let physicianData = {
       "type":1,
         "physicianEmail": "", //sessionStorage.getItem("email"),
         "isBooked":false
     };
+    console.log("PAPA");
+    console.log(sessionStorage.getItem("email"));
     let schedulesToShow: { title: string, start: string, end: string, constraint: string }[] = []; // elimine el date: string
       try {
         this.http.get("http://localhost:4000/appointments").subscribe((resultData:any)=>{
           
-          //console.log("ESTO ES RESULDATA:");
-          //console.log(resultData);// modifique el .data
-          let schedules=resultData; // aqui tambien lo modifique
-          
+          console.log("ESTO ES RESULDATA:");
+          console.log(resultData);
+          let schedules=resultData; 
+          console.log("Accediendo a is_booked");
+          console.log(schedules.is_booked);
           
           for(let i=0; i < schedules.length; i++){
-           // console.log(schedules.length);
-            //console.log(schedules[i].date);
-            let splitedDate = schedules[i].date.split("T");  // le agregue el .split();
+           
+            let splitedDate = schedules[i].date.split("T");  
             
   
             let showAppointments={
-              title: "Available",
-              // date: splitedDate,        comente esto 
+              title: schedules[i].is_booked ? "Booked" : "Available",
+             
               start: splitedDate[0]+"T"+schedules[i].start_time+":00", 
-              end: splitedDate[0]+"T"+schedules[i].end_time+":00",   // elimino los
+              end: splitedDate[0]+"T"+schedules[i].end_time+":00", 
+              color: schedules[i].is_booked ? 'red' : 'green',  
               constraint: 'businessHours' 
             };
   
@@ -103,31 +102,12 @@ export class LoadPhysicianAvailabilityComponent {
           console.log("Data transformed:");
           console.log(schedulesToShow);
           this.calendarOptions.events=schedulesToShow;
+          console.log("ESTO ME DARA EL RESULTADO: ");
+          console.log(this.calendarOptions.events);
+
+          
           
         });
-
-        // Traer citas de los pacientes
-        /*physicianData.isBooked=true;
-      this.http.put("http://localhost:4000/appointments/schedule/:id", physicianData).subscribe((resultData:any)=>{
-        console.log(resultData);  
-
-        let schedules2=resultData.message;
-        for(let i=0; i < schedules2.length; i++){
-          let splitedDate2 = schedules2[i].Date.split("T");  
-
-          let showAppointments={
-            title: schedules2[i].patientEmail,
-            start: splitedDate2[0]+"T"+schedules2[i].startTime+":00", 
-            end: splitedDate2[0]+"T"+schedules2[i].endTime+":00",   
-            constraint: 'businessHours', 
-            backgroundColor: 'red'
-          };
-          schedulesToShow.push(showAppointments);
-        }
-        console.log("Data transformed2:");
-        console.log(schedulesToShow);
-        this.calendarOptions.events=schedulesToShow;
-      });*/
 
     } catch (error) {
       console.error(error);
@@ -144,32 +124,3 @@ export class LoadPhysicianAvailabilityComponent {
  
 
 
-// Función para obtener eventos desde el backend
-  /*obtenerEventos(): void {
-    const backendURL = 'http://localhost:5000'; // Reemplaza con la URL de tu backend
-
-    this.http.get<any[]>(`${backendURL}/books`).subscribe(
-      (response) => {
-        console.log('Eventos cargados exitosamente:', response);
-
-        // Verifica si la propiedad 'data' es un array antes de asignar a eventos
-        if (Array.isArray(response)) {
-          const eventos = response.map(libro => ({
-            title: 'Disponible',
-            start: libro.date,
-            end: libro.date,
-            constraint: 'businessHours'
-          }));
-
-          // Actualiza los eventos del calendario
-          this.calendarOptions.events = eventos;
-          this.calendarComponent.getApi().refetchEvents();
-        } else {
-          console.error('Error: La respuesta no es un array/////');
-        }
-      },
-      (error) => {
-        console.error('Error al cargar eventos:', error);
-      }
-    );
-  }*/
